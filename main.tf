6989ba6382d0c2f8d1023b9c9444e461b714475a
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-11-13 21:02:33 -0500

    start roboshop project

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ variable "components"{
 }
 
 resource "aws_instance" "instance" {
-  count         = length(var.components)
-  ami           =  "ami-09c813fb71547fc4f from us-east-1"
-  instance_type = "t3.small"
+  count                  = length(var.components)
+  ami                    =  "ami-02082c2bb2f49f709"
+  instance_type          = "t3.small"
   vpc_security_group_ids = ["sg-0719fd3602de52422"]
   tags = {
     Name= var.components[count.index]
